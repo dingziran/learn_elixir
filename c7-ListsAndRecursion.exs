a58d1095958e722d57da271693956be38eaef1a3
dingziran <dingziranrr@163.com>
2015-11-05 14:51:48 +0800

    finished take split method

--- c7-ListsAndRecursion.exs
+++ c7-ListsAndRecursion.exs
@@ -23,15 +23,33 @@ defmodule MyList do
     do: [from| span(from+1,to)]
   def span(to,to),
     do: [to]
+  
   def all?([],func), do: true
   def all?([head|tail],func), do:
     func.(head) && all?(tail,func)        
+
   def each([],_func), do: [] 
   def each([head|tail],func), do:
     [func.(head) | each(tail,func)]
 
-  #def filter
-  #def split
-  #def take
+  def filter([],_func), do: []
+  def filter([head|tail],func) do
+    if func.(head) do
+      [head|filter(tail,func)]
+    else
+      filter( tail , func )
+    end
+  end
+  
+  def take([],n), do: []
+  def take(_,n) when n <= 0, do: []
+  def take([ head| tail], n), do: [ head| take( tail, n-1)]
+
+  def rest([],n), do: []
+  def rest( list, n) when n<=0, do: list
+  def rest([head | tail], n), do: rest( tail, n-1)
+
+
+  def split(list, n), do: {take(list, n), rest( list, n)}
 
 end
